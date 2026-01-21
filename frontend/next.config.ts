22db951ad5682e4cfcf5f38d80f2db44bcf34555
mishop-15 <tanaybhutada03@gmail.com>
2026-01-21 18:24:47 +0530

    resolved hydration warning and next config types

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    } else {
+      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    }
+    config.experiments = { ...config.experiments, asyncWebAssembly: true, layers: true };
+    return config;
+  },
+  serverExternalPackages: ['@aztec/bb.js', '@noir-lang/backend_barretenberg'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/@aztec/bb.js/**/*.wasm'], 
+    '/**/*': ['./node_modules/@aztec/bb.js/**/*.wasm'],     
+  },
 };
 
 export default nextConfig;
